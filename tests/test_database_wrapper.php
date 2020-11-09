8ca1d02aee76d8774ea9b0c7aeca5568d7205d5c
Francisco de Assis Boldt <fboldt@gmail.com>
2020-11-09 13:29:04 -0300

    limpeza de arquivos desnecessários

--- tests/test_database_wrapper.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../database/database_wrapper.php';
-function test_database_wrapper() {
-    $databaserWrapper = new DatabaseWrapper();
-    print_r($databaserWrapper->selectAll("notesview"));
-}
-?>
--- tests/test_localhost.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../database/localhost.php';
-
-function test_localhost() {
-    $mysqlConnection = DBMSConnection();
-    print_r($mysqlConnection);
-}
-// test();
-?>
