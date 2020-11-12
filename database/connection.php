769195c81480f7bb94465743dbb1530c1d07ad23
Francisco de Assis Boldt <fboldt@gmail.com>
2020-11-12 13:46:08 -0300

    organize connection requires on necessity

--- database/connection.php
+++ database/connection.php
@@ -1,10 +1,4 @@
 <?php
-require_once 'localMysqlCredentials.php';
-require_once 'remoteMysqlCredentials.php';
-require_once 'mysqlConnection.php';
-require_once 'localPostgresCredentials.php';
-require_once 'remotePostgresCredentials.php';
-require_once 'postgresConnection.php';
 
 function runningLocal() {
     return ($_SERVER['SERVER_ADDR'] == "::1" || $_SERVER['SERVER_ADDR'] == "127.0.0.1");
@@ -12,16 +6,22 @@ function runningLocal() {
 
 function getDatabaseConnection() {
     //*
+    require_once 'postgresConnection.php';
     if (runningLocal()) {
+        require_once 'localPostgresCredentials.php';
         $postgresCredentials = new LocalPostgresCredentials();
     } else {
+        require_once 'remotePostgresCredentials.php';
         $postgresCredentials = new RemotePostgresCredentials();
     }
     return new PostgresConnection($postgresCredentials);
     /*/
+    require_once 'mysqlConnection.php';
     if (runningLocal()) {
+        require_once 'localMysqlCredentials.php';
         $mysqlCredentials = new LocalMysqlCredentials();
     } else {
+        require_once 'remoteMysqlCredentials.php';
         $mysqlCredentials = new RemoteMysqlCredentials();
     }
     return new MysqlConnection($mysqlCredentials);
